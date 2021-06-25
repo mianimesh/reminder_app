819d037e262cf6b95dc5ddfca08485efdee5b052
mianimesh <animeshbabu.mishra@gmail.com>
2021-06-25 22:49:09 +0530

    reminder

--- App.js
+++ App.js
@@ -1,21 +1,127 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform,TextInput } from 'react-native';
+import {Input} from 'react-native-elements'; 
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound:true,
+    shouldSetBadge: false
+  }),
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const [title,setTitle] = useState('title') ; 
+  const [body,setBody] = useState('body') ;  
+  const [isDatePickerVisible,setDatePickerVisibility] = useState(false) ; 
+  const [seconds,setSeconds] = useState(1) ; 
+
+  const showDatePicker = ()=>{
+    setDatePickerVisibility(true) ; 
+  }
+  const hideDatePicker = ()=>{
+    setDatePickerVisibility(false) ; 
+  }
+  const handleConfirm = (datetime)=>{
+    let current = new Date() ; 
+    setSeconds((datetime-current)/1000) ; 
+    if((datetime-current<0))
+      alert('Please Select Correct Date and Time in Future! ')
+    hideDatePicker() ; 
+  }
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      {/* <Text>Your expo push token: {expoPushToken}</Text> */}
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        
+        <Input label='Reminder Title' onChangeText={(value)=>setTitle(value)}/>
+        <Input label='Reminder Body' onChangeText={(value)=>setBody(value)}/>
+        <Button title='Set Date and Time' onPress={showDatePicker}/>
+        <DateTimePickerModal isVisible = {isDatePickerVisible} mode='datetime' onConfirm={handleConfirm} onCancel={hideDatePicker}/>
+      </View>
+      
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification({title,body,seconds});
+          setTitle('');
+          setBody('');
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+async function schedulePushNotification(props) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: props.title, 
+      body: props.body,
+      sound:'sound.wav'
+    },
+    trigger: {
+      seconds:props.seconds 
+    }
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+      sound:'sound.wav',
+    });
+  }
+
+  return token;
+}
